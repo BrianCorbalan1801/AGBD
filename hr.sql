8ac902d3eb6b7abe2bd4b29bdcf7a51f2135b57c
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-04-10 11:51:19 -0300

    Empezando el HR

---
+++ hr.sql
@@ -0,0 +1,38 @@
+/*Actividad 1*/
+
+SELECT first_name, last_name, salary from employees
+where salary < 6000
+
+/*Actividad 2*/
+
+
+
+/*Actividad 3*/
+
+
+
+/*Actividad 4*/
+
+
+
+/*Actividad 5*/
+
+
+
+/*Actividad 6*/
+
+
+
+/*Actividad 7*/
+
+
+
+/*Actividad 8*/
+
+
+
+/*Actividad 9*/
+
+
+
+/*Actividad 10*/
