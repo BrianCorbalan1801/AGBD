6261e165884c39dce22bac9533911ce773786f35
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-02 11:27:20 -0300

    Completado el 3er punto del simulacro

--- Simulacro_Evaluacion.sql
+++ Simulacro_Evaluacion.sql
@@ -5,10 +5,14 @@ ORDER BY title ASC
 
 -- Actividad 2 --
 
-
+SELECT DISTINCT rating FROM film 
+ORDER BY rating ASC
 
 -- Actividad 3 --
 
 
+
+SELECT title, rental_rate FROM film 
+ORDER BY title ASC
 
 -- Actividad 4 --
