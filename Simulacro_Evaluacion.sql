b3271a0563dce911ec2f1b26fca6513d3457f3eb
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-02 11:55:57 -0300

    Completado el 5to punto del simulacro

--- Simulacro_Evaluacion.sql
+++ Simulacro_Evaluacion.sql
@@ -19,3 +19,13 @@ ORDER BY rental_rate DESC, title ASC;
 SELECT city.city, country.country FROM city
 JOIN country ON city.country_id = country.country_id
 ORDER BY country.country ASC, city.city ASC;
+
+-- Actividad 5 --
+
+SELECT count(*) as cant_ciudad, p.country FROM city c
+JOIN country p ON c.country_id = p.country_id
+GROUP by p.country_id
+ORDER by cant_ciudad DESC
+
+-- Actividad 6 --
+
