3f4558f1fbbb706912152d50dca6e11723e72892
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-08 11:16:04 -0300

    Actividad 1 y 2 realizada

---
+++ Simulacro-Evaluacion-Hr.sql
@@ -0,0 +1,27 @@
+--Actividad 1--
+
+SELECT department_name from departments
+order by department_name ASC
+
+--Actividad 2--
+
+SELECT DISTINCT salary from employees
+ORDER by salary DESC
+
+--Actividad 3--
+
+
+
+--Actividad 4--
+
+
+
+--Actividad 5--
+
+
+
+--Actividad 6--
+
+
+
+--Actividad 7--
