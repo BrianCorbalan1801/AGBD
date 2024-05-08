997523409c9ff2352d37d96a63ca1b90e00f75f6
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-08 12:00:48 -0300

    Actividad 4 realizada

--- Simulacro-Evaluacion-Hr.sql
+++ Simulacro-Evaluacion-Hr.sql
@@ -16,11 +16,17 @@ ORDER by min_salary DESC
 
 --Actividad 4--
 
-
+SELECT r.region_name, country_name FROM countries c
+JOIN  regions r on c.region_id = r.region_id
+ORDER by region_name ASC 
 
 --Actividad 5--
 
-
+SELECT e.first_name, e.last_name, d.department_name, e.salary FROM employees e
+JOIN departments d on e.department_id = d.department_id
+WHERE salary BETWEEN 9000 and 17000
+ORDER by salary ASC
+ 
 
 --Actividad 6--
 
