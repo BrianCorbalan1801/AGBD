a240ce4dba9718aa96b2332a83dfe33668879860
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-16 10:38:13 -0300

    Resuelto el ejercicio 2 de Evaluacion

--- Evaluacion /Ejercicio-2.sql
+++ Evaluacion /Ejercicio-2.sql
@@ -3,3 +3,6 @@
 -- todos las mujeres de menos de 40 años, 
 -- elegir el orden en que se muestran 
 
+SELECT name, car_model, car_make, plate_number FROM drivers_license d
+JOIN person p ON d.id = p.license_id
+ORDER BY name ASC
