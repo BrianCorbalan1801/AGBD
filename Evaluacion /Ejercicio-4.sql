a252bf1ea7649d85fe0cc196a7dc2b3f3fd36137
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-05-16 11:16:16 -0300

    Me saltee actividad 3 por no entender y resolvi la actividad 4

--- Evaluacion /Ejercicio-4.sql
+++ Evaluacion /Ejercicio-4.sql
@@ -3,3 +3,7 @@
 -- de la calle 'Franklin Ave' (ordenadas por la altura
 -- de la calle)
 
+SELECT name, address_street_name, address_number from person 
+WHERE address_street_name = "Franklin Ave"
+ORDER BY address_number ASC
+LIMIT 1 OFFSET 2
