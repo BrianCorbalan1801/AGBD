231aa0049f6ff27e729343dc6370833aa7e63467
Briancorbalan1801 <Bcorbalanbenitez@gmail.com>
2024-06-27 11:39:26 -0300

    Completado la actividad 7 de la evaluacion. (esta si es la 7)

--- Evaluacion HR/Evaluacion.sql
+++ Evaluacion HR/Evaluacion.sql
@@ -75,3 +75,6 @@ WHERE country_name = 'Canada';
 -- Como último requerimiento nos solicitan eliminar todos los trabajos que no posean empleados asociados, 
 -- la query tiene que tener la capacidad de eliminar todos los jobs que no posean empleados en una sola 
 -- ejecución sin indicar de manera manual el numero los jobs_id a eliminar.
+
+DELETE FROM jobs
+WHERE job_id NOT IN (SELECT DISTINCT job_id FROM employees);
